163a3f9857162bd047eb0951181bf51630a2cafe
SheilaNieva <sheilanievalo.2000@gmail.com>
2021-05-14 16:56:24 +0200

    carpetas ui

---
+++ app/src/main/java/com/sheilalberto/alquilsa/ui/miUbicacion/MiUbicacionFragment.kt
@@ -0,0 +1,27 @@
+package com.sheilalberto.alquilsa.ui.miUbicacion
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.sheilalberto.alquilsa.R
+
+class MiUbicacionFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_mi_ubicacion, container, false)
+
+        return root
+    }
+}
